2fd4ffedcae898890cb19bea3ca29df250b72094
ilai2 <isabel_lai@brown.edu>
2021-01-18 13:45:27 -0500

    Changed 2020 to 2021 on bignum due date, moved around resources page and added video guides and links to diversity&inclusion and health&wellness advocates

--- src/Info/Resources/Resources.js
+++ src/Info/Resources/Resources.js
@@ -22,13 +22,27 @@ export const Resources = [
 	{ section: "CS17 Language Guides", links: 
 		[
 			{ label: "General Style Guide", link: "https://drive.google.com/file/d/1vpQYJOJzFr5lUkodyuxzn_u-KVXAMAX1/view?usp=sharing" }, 
-			{ label: "Racket Style Guide", link: "https://drive.google.com/file/d/1CHuEpQ01N6LSUBeyZer5Di1QxFDIUsvl/view?usp=sharing" },
 			{ label: "Racket to ReasonML", link: "https://drive.google.com/file/d/1kk7qKwy7NLLZx-gqyZRSbw9MiTF5ExVP/view?usp=sharing" }, 
-			{ label: "ReasonML Style Guide", link: "https://drive.google.com/file/d/1olLwJEND7l6UR_NvehxaG1yzdPq3lmZ6/view?usp=sharing" },
+			{ label: "Racket Style Guide", link: "https://drive.google.com/file/d/1CHuEpQ01N6LSUBeyZer5Di1QxFDIUsvl/view?usp=sharing" },
 			{ label: "ReasonML Debugging", link: "https://drive.google.com/file/d/1SvePYEiHwqP6DMkdAcrus8UEZg3oe3QC/view?usp=sharing" }, 
-			{ label: "CS17 Racket Built-in Procedures and Data", link: "https://drive.google.com/file/d/1Aosl7flOkF0jdolTfrDABwrBVp2SSiQI/view?usp=sharing" },
+			{ label: "ReasonML Style Guide", link: "https://drive.google.com/file/d/1olLwJEND7l6UR_NvehxaG1yzdPq3lmZ6/view?usp=sharing" }
+			// { label: "CS17 Racket Built-in Procedures and Data", link: "https://drive.google.com/file/d/1Aosl7flOkF0jdolTfrDABwrBVp2SSiQI/view?usp=sharing" },
 		]
 	},
+	{ section: "Working from Home", links: 
+		[
+			// { label: "Work from Home Guide", link: "https://cs.brown.edu/about/system/connecting/ssh/" }, 
+			// { label: "SSH Guide", link: "https://drive.google.com/file/d/1PD9C5Bh7uT0dsLY0dhryPF5Kc1KG0GO_/view?usp=sharing" },
+			{ label: "Download DrRacket", link: "https://racket-lang.org/download/" }, 
+			{ label: "ReasonML Installation and Compilation Guide", link: "https://drive.google.com/file/d/15EEZHR86owNFpyPWo2gVkimf4XrrrdOW/view?usp=sharing" },
+			{ label: "CS17 Racket Installation Video Guide (Mac)", link: "https://drive.google.com/file/d/1wy482mD0ZCqfl8Jcz_25sKV7FeBBGML6/view?usp=sharing" },
+			{ label: "CS17 ReasonML Installation Video Guide (Mac)", link: "https://drive.google.com/file/d/1OS-7KPUnBxAquAZsez1L7N_RhjEjtjoT/view?usp=sharing" },
+			{ label: "CS17 Racket Installation Video Guide (Windows)", link: "https://drive.google.com/file/d/1GfrL5xz7HQhBbkTxKvHJ5yH4lfQk3lpv/view?usp=sharing" },
+			{ label: "CS17 ReasonML Installation Video Guide (Windows)", link: "" },
+			{ label: "CS17 Racket Teachpack", link: "https://drive.google.com/file/d/1Yc0e6P3WH-2iSKeCHqTImzAME13CkEAM/view?usp=sharing" },
+			// { label: "CS17 ReasonML Teachpack", link: "https://drive.google.com/file/d/1v31kqpdPbmUX15kUSTwcp2hm0hYv_a35/view?usp=sharing" }, 
+		]
+	},	
 	{ section: "Supplementary Material", links: 
 		[
 			{ label: "Ergonomics Guide", link: "https://cs.brown.edu/systems/ergo/" }, 
@@ -39,14 +53,11 @@ export const Resources = [
 			{ label: "ReasonML Library Reference", link: "https://reasonml.github.io/docs/en/overview" },
 		]
 	},
-	{ section: "Working from Home", links: 
+	{ section: "Departmental Resources", links:
 		[
-			{ label: "Work from Home Guide", link: "https://cs.brown.edu/about/system/connecting/ssh/" }, 
-			{ label: "SSH Guide", link: "https://drive.google.com/file/d/1PD9C5Bh7uT0dsLY0dhryPF5Kc1KG0GO_/view?usp=sharing" },
-			{ label: "Download DrRacket", link: "https://racket-lang.org/download/" }, 
-			{ label: "CS17 Racket Teachpack", link: "https://drive.google.com/file/d/1Yc0e6P3WH-2iSKeCHqTImzAME13CkEAM/view?usp=sharing" },
-			{ label: "CS17 ReasonML Teachpack", link: "https://drive.google.com/file/d/1v31kqpdPbmUX15kUSTwcp2hm0hYv_a35/view?usp=sharing" }, 
-			{ label: "ReasonML Installation and Compilation Guide", link: "https://drive.google.com/file/d/15EEZHR86owNFpyPWo2gVkimf4XrrrdOW/view?usp=sharing" },
+			{ label: "Diversity and Inclusion Student Advocates", link: "https://cs.brown.edu/about/diversity/student-advocates-diversity-and-inclusion/" },
+			{ label: "Health and Wellness Student Advocates", link: "https://cs.brown.edu/about/diversity/health-wellness-student-advocates/" },
+			{ label: "Other Resources", link: "http://cs.brown.edu/about/diversity/resources/" },
 		]
-	},
+	}
 ]
