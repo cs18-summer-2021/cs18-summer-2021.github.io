c0683a33d770f83cc22a5523e200ec11d5e6479f
ilai2 <isabel_lai@brown.edu>
2021-01-17 23:30:39 -0500

    Made hw and projects cards visible, updated due dates

--- src/Info/Assignments/Homeworks/Homeworks.js
+++ src/Info/Assignments/Homeworks/Homeworks.js
@@ -8,83 +8,76 @@ const homeworks = [
 			{ icon: <MdCode/>, link: "", label: "Source Code" },
 		]
 	},
-	// { 
-	// 	number: 2, title: "Functions & Procedures", dueDate: "01-21-2021", active: true,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 3, title: "Recursion", dueDate: "01-21-2021", active: true,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 4, title: "More Recursion", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 5, title: "Higher-Order Procedures", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 6, title: "Subsets", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 7, title: "ReasonML/Matrices", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 8, title: "Rackettecita", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 9, title: "Abstract Data Types", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 10, title: "Trees", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 11, title: "Divide and Conquer", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 12, title: "BSTs and Accumulators", dueDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
+	{ 
+		number: 2, title: "Functions & Procedures", dueDate: "02-03-2021", active: true,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 3, title: "Recursion", dueDate: "02-10-2021", active: true,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 4, title: "More Recursion", dueDate: "02-19-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 5, title: "Higher-Order Procedures", dueDate: "02-24-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 6, title: "Subsets", dueDate: "03-03-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 7, title: "ReasonML/Matrices", dueDate: "03-10-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 8, title: "Rackettecita", dueDate: "03-17-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 9, title: "Homework 9", dueDate: "03-24-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 10, title: "Homework 10", dueDate: "03-31-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 11, title: "Homework 11", dueDate: "04-07-2021", active: false,
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	}
 ]
 
 export default homeworks;
--- src/Info/Assignments/Projects/Projects.js
+++ src/Info/Assignments/Projects/Projects.js
@@ -9,22 +9,22 @@ const projects = [
 			{ icon: <MdCode/>, link: "", label: "Source Code" },
 		]
 	},
-	// { 
-	// 	number: 2, title: "Rackette", dueDate: "01-21-2021", active: false,
-	// 	notes: "",
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
-	// { 
-	// 	number: 3, title: "Game", dueDate: "01-21-2021", active: false,
-	// 	notes: "",
-	// 	links: [
-	// 		{ icon: <MdDescription/>, link: "", label: "Handout" },
-	// 		{ icon: <MdCode/>, link: "", label: "Source Code" },
-	// 	]
-	// },
+	{ 
+		number: 2, title: "Rackette", dueDate: "03-26-2021", active: false,
+		notes: "",
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	},
+	{ 
+		number: 3, title: "Game", dueDate: "04-08-2021", active: false,
+		notes: "",
+		links: [
+			{ icon: <MdDescription/>, link: "", label: "Handout" },
+			{ icon: <MdCode/>, link: "", label: "Source Code" },
+		]
+	}
 ]
 
 export default projects;
