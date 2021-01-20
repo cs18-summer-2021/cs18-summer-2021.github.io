f1724bad18bfa4a98d75be7764a2e754009de11b
TimothyWang56 <timothywang56@gmail.com>
2021-01-20 12:49:12 -0500

    add class 1 links

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -2,11 +2,11 @@ import { MdDescription, MdMovie, MdSlideshow } from 'react-icons/md';
 
 const classes = [
 	{ 
-		number: 1, title: "Intro", dueDate: "01-20-2021", active: false,
+		number: 1, title: "Intro", dueDate: "01-20-2021", active: true,
 		links: [
-			{ icon: <MdDescription/>, link: "" },
-			{ icon: <MdMovie/>, link: "" },
-			{ icon: <MdSlideshow/>, link: "" },
+			{ icon: <MdDescription/>, link: "https://drive.google.com/file/d/1WwjwOg_BqxoTCqSYvFrWZlUIXMyrv8Q2/view?usp=sharing" },
+			{ icon: <MdMovie/>, link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5407b5b3-9976-4b50-bcd1-acb6011d1f9c" },
+			{ icon: <MdSlideshow/>, link: "https://drive.google.com/file/d/1yDSxs9cmu0kmaiuWbqdovtrhtpTLC1WT/view?usp=sharing" },
 		],
 		tags: ["Intro", "Racket"]
 	},
