69c1e1f22eddb72e74ef1a3cb0dee17c0eb142cd
ilai2 <isabel_lai@brown.edu>
2021-01-22 12:50:54 -0500

    Added class 2

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -1,6 +1,24 @@
 import { FaFileAlt, FaYoutube, FaFilePowerpoint } from 'react-icons/fa';
 
 const classes = [
+	// { 
+	// 	number: 3, title: "Class 3", outDate: "1/25/2021", active: true,
+	// 	links: [
+	// 		{ icon: <FaFileAlt/>, link: "", label: "Notes" },
+	// 		{ icon: <FaYoutube/>, link: "", label: "Recording" },
+	// 		{ icon: <FaFilePowerpoint/>, link: "", label: "Slides" },
+	// 	],
+	// 	tags: ["Racket"]
+	// },
+	{ 
+		number: 2, title: "BNF and Racket", outDate: "1/22/2021", active: true,
+		links: [
+			{ icon: <FaFileAlt/>, link: "https://drive.google.com/file/d/1F_EDAtYqw-DW_M1fgBbuILy2s9vowY6H/view?usp=sharing", label: "Notes" },
+			{ icon: <FaYoutube/>, link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=6ea4ad77-a7f7-4c7d-ba53-acb8011c934d", label: "Recording" },
+			{ icon: <FaFilePowerpoint/>, link: "https://drive.google.com/file/d/1_gyZi5469rr-RotMh-Od8sbm3QMuc7CK/view?usp=sharing", label: "Slides" },
+		],
+		tags: ["Racket", "BNF"]
+	},
 	{ 
 		number: 1, title: "Welcome", outDate: "1/20/2021", active: true,
 		links: [
@@ -10,15 +28,6 @@ const classes = [
 		],
 		tags: ["Intro", "Racket"]
 	},
-	// { 
-	// 	number: 2, title: "lect2", outDate: "01-21-2021", active: true,
-	// 	links: [
-	// 		{ icon: <FaFileAlt/>, link: "" },
-	// 		{ icon: <MdMovie/>, link: "" },
-	// 		{ icon: <FaFilePowerpoint/>, link: "" },
-	// 	],
-	// 	tags: ["Racket"]
-	// },
 ]
 
 export default classes;
