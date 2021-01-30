954fa9b288267f44d2e42cb845c29e975bb4c695
ilai2 <isabel_lai@brown.edu>
2021-01-30 16:08:10 -0500

    Added workshop 2

--- src/Info/Notes/Workshops/Workshops.js
+++ src/Info/Notes/Workshops/Workshops.js
@@ -2,17 +2,17 @@ import { FaFilePowerpoint } from "react-icons/fa";
 
 const workshops = [
 	{ 
+		number: 2, title: "Expressions, Types, and Procedures", outDate: "1/29 - 1/30/2021", active: true,
+		links: [
+			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1ZDpJO509XxH8qsu2HnPc7qV3zRCXuQdS0x_QAz-3_Xs/edit?usp=sharing", label: "Slides" },
+		]
+	},
+	{ 
 		number: 1, title: "Learning CS and Fun with DrRacket", outDate: "1/23/2021", active: true,
 		links: [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/13YCPuSVxMijjb-KoQMlPngVf5g_jh122XAMQ4TlJvcw/edit?usp=sharing", label: "Slides" },
 		]
 	},
-	// { 
-	// 	number: 2, title: "workshop2", outDate: "01-21-2021", active: false,
-	// 	links: [
-	// 		{ icon: <FaFilePowerpoint/>, link: "" },
-	// 	]
-	// },
 ]
 
 export default workshops;
