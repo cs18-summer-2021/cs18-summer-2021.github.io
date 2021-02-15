42b242ec416b177d032558407ad62150ad4f8aa3
ilai2 <isabel_lai@brown.edu>
2021-02-15 14:13:28 -0500

    workshop 4

--- src/Info/Notes/Workshops/Workshops.js
+++ src/Info/Notes/Workshops/Workshops.js
@@ -2,6 +2,12 @@ import { FaFilePowerpoint } from "react-icons/fa";
 
 const workshops = [
 	{
+		number: 4, title: "Recurrence Relations", outDate: "2/12 - 2/13/2021", active: true,
+		links: [
+			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/15SsYVNs1aV2Eya2-brOkwKRyhKtA6Yolu7z84wzVxA0/edit?usp=sharing", label: "Slides" },
+		]
+	},
+	{
 		number: 3, title: "Boolean Logic, Lists, and Recursion", outDate: "2/5 - 2/6/2021", active: true,
 		links: [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1F3rYDxbBOER6mRnSLvLWqPZlKtMUGkcCjLijX3R10go/edit?usp=sharing", label: "Slides" },
