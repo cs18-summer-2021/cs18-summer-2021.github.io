1405eefdadfdd1d15bb9151abb56390207edd289
Sohum Gupta <sohumgupta99@gmail.com>
2021-02-18 17:13:30 -0500

    Change links to open in new tab

--- src/Components/IconLink/IconLink.js
+++ src/Components/IconLink/IconLink.js
@@ -4,7 +4,7 @@ import './IconLink.scss';
 class IconLink extends React.Component {
 	render() {
 		return (
-			<a className="icon-link" href={this.props.link} title={this.props.label}>
+			<a className="icon-link" href={this.props.link} target="_blank" rel="noopener noreferrer" title={this.props.label}>
 				{this.props.icon}
 				<p className="icon-tooltip">{this.props.label}</p>
 			</a>
