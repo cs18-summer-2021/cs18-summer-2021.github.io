b2698d89de3df28e2c6339b8dc307e25f76e22e1
ilai2 <isabel_lai@brown.edu>
2021-02-27 20:37:00 -0500

    Workshop 6

--- src/Info/Notes/Workshops/Workshops.js
+++ src/Info/Notes/Workshops/Workshops.js
@@ -2,6 +2,12 @@ import { FaFilePowerpoint } from "react-icons/fa";
 
 const workshops = [
 	{
+		number: 6, title: "Let Expressions and Intro to ReasonML", outDate: "2/26 - 2/27/2021", active: true,
+		links: [
+			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1y6T1iZY_0-X0uRM2UgOpU92taVSzeU0HvHMVhPZr2lY/edit?usp=sharing", label: "Slides" },
+		]
+	},
+	{
 		number: 5, title: "Analysis, Lambda Expressions, and HOPs", outDate: "2/19 - 2/20/2021", active: true,
 		links: [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1T5gniCdUjZDYBm-QCMQOAuuHWnqtxsI3dkKE8Sp4vBo/edit?usp=sharing", label: "Slides" },
@@ -19,13 +25,13 @@ const workshops = [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1F3rYDxbBOER6mRnSLvLWqPZlKtMUGkcCjLijX3R10go/edit?usp=sharing", label: "Slides" },
 		]
 	},
-	{ 
+	{
 		number: 2, title: "Expressions, Types, and Procedures", outDate: "1/29 - 1/30/2021", active: true,
 		links: [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/1ZDpJO509XxH8qsu2HnPc7qV3zRCXuQdS0x_QAz-3_Xs/edit?usp=sharing", label: "Slides" },
 		]
 	},
-	{ 
+	{
 		number: 1, title: "Learning CS and Fun with DrRacket", outDate: "1/23/2021", active: true,
 		links: [
 			{ icon: <FaFilePowerpoint/>, link: "https://docs.google.com/presentation/d/13YCPuSVxMijjb-KoQMlPngVf5g_jh122XAMQ4TlJvcw/edit?usp=sharing", label: "Slides" },
