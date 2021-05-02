c5bedeff7d44539e2a9a283a776c5536a61ade42
ilai2 <isabel_lai@brown.edu>
2021-05-01 20:33:29 -0500

    gaya's picture

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -2,13 +2,13 @@ import { FaFileAlt, FaYoutube, FaFilePowerpoint } from 'react-icons/fa';
 
 const classes = [
 	{
-		number: 1, title: "Welcome", outDate: "1/20/2021", active: false,
+		number: 1, title: "Welcome", outDate: "5/12/2021", active: false,
 		links: [
-			{ icon: <FaFileAlt />, link: "https://drive.google.com/file/d/1WwjwOg_BqxoTCqSYvFrWZlUIXMyrv8Q2/view?usp=sharing", label: "Notes" },
-			{ icon: <FaYoutube />, link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5407b5b3-9976-4b50-bcd1-acb6011d1f9c", label: "Recording" },
-			{ icon: <FaFilePowerpoint />, link: "https://drive.google.com/file/d/1yDSxs9cmu0kmaiuWbqdovtrhtpTLC1WT/view?usp=sharing", label: "Slides" },
+			{ icon: <FaFileAlt />, link: "", label: "Notes" },
+			{ icon: <FaYoutube />, link: "", label: "Recording" },
+			{ icon: <FaFilePowerpoint />, link: "", label: "Slides" },
 		],
-		tags: ["Java"]
+		tags: []
 	},
 ]
 
