30eee130fa5e53f770c13db693cfa634422f506c
Kathi Fisler <kfisler@gmail.com>
2021-05-11 21:01:58 -0400

    added lec1 setup

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -2,11 +2,12 @@ import { FaFileAlt, FaYoutube, FaFilePowerpoint } from 'react-icons/fa';
 
 const classes = [
 	{
-		number: 1, title: "Welcome", outDate: "May 12", active: false,
+		number: 1, title: "Intro and Classes", outDate: "May 12", active: true,
 		links: [
-			{ icon: <FaFileAlt />, link: "", label: "Notes" },
-			{ icon: <FaYoutube />, link: "", label: "Recording" },
-			{ icon: <FaFilePowerpoint />, link: "", label: "Slides" },
+		    { icon: <FaFileAlt />, link: "https://brown-cs18-master.github.io/content/lectures/01intro/dillo.ml", label: "Setup/Prep" },
+		    // { icon: <FaFileAlt />, link: "", label: "Notes" },
+		    // { icon: <FaYoutube />, link: "", label: "Recording" },
+		    // { icon: <FaFilePowerpoint />, link: "", label: "Slides" },
 		],
 		tags: []
 	},
