2982888293820baec1471000a25627e1277e48ff
Kathi Fisler <kfisler@gmail.com>
2021-05-12 12:45:07 -0400

    lec1 notes/video, lec2 setup

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -1,13 +1,33 @@
 import { FaFileAlt, FaYoutube, FaFilePowerpoint } from 'react-icons/fa';
 
 const classes = [
-	{
+    {
 		number: 1, title: "Intro and Classes", outDate: "May 12", active: true,
 		links: [
-		    { icon: <FaFileAlt />, link: "https://brown-cs18-master.github.io/content/lectures/01intro/dillo.ml", label: "Setup/Prep" },
-		    // { icon: <FaFileAlt />, link: "", label: "Notes" },
-		    // { icon: <FaYoutube />, link: "", label: "Recording" },
-		    // { icon: <FaFilePowerpoint />, link: "", label: "Slides" },
+		    { icon: <FaFileAlt />,
+		      link: "https://brown-cs18-master.github.io/content/lectures/01intro/dillo.ml",
+		      label: "Setup/Prep" },
+		    { icon: <FaFileAlt />,
+		      link: "https://cs18-summer-2021.github.io/src/Info/Notes/Classes/lec01.pdf",
+		      label: "Notes" },
+		    { icon: <FaYoutube />,
+		      link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa",
+		      label: "Video" },
+		],
+		tags: []
+    },
+    {
+		number: 2, title: "Methods and Testing", outDate: "May 14", active: true,
+		links: [
+		    { icon: <FaFileAlt />,
+		      link: "https://hackmd.io/@cs18-spring-2021/lec02setup",
+		      label: "Setup/Prep" },
+		    //{ icon: <FaFileAlt />,
+		    //  link: "https://cs18-summer-2021.github.io/src/Info/Notes/Classes/lec01.pdf",
+		    //  label: "Notes" },
+		    //{ icon: <FaYoutube />,
+		    //  link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa",
+		    //  label: "Video" },
 		],
 		tags: []
 	},
