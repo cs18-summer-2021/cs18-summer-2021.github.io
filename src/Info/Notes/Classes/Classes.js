e03ebb31665f0488cd8574ebd90df45ed5549162
Ian Layzer <ianlayzer@gmail.com>
2021-05-13 16:23:46 -0400

    Made helper functions for making icon links in the classes page

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -1,37 +1,72 @@
-import { FaFileAlt, FaYoutube, FaFilePowerpoint, FaPencilRuler, FaWrench, FaBook } from 'react-icons/fa';
+import { FaFileAlt, FaYoutube, FaFilePowerpoint, FaPencilRuler, FaWrench, FaBook, FaMapMarkedAlt, FaCogs, FaMap } from 'react-icons/fa';
 
 const staticBasePath = "/static/classes";
+
+const iconType = {
+	SETUP: "Setup/Prep",
+	NOTES: "Notes",
+	VIDEO: "Video",
+	PRACTICE: "Practice"
+}
+
+/**
+ * Makes an IconLink for a Class card.
+ * @param {iconType} type the type of icon to make (see above definition) 
+ * @param {string} url the url to link to
+ * @returns an icon link of a certain type linking to the given url
+ */
+function makeIcon(type, url) {
+	let iconToUse // default icon if provided type unknown
+	switch (type) {
+		case iconType.SETUP:
+			iconToUse = <FaMap />
+			break;
+		case iconType.NOTES:
+			iconToUse = <FaFileAlt />
+			break;
+		case iconType.VIDEO:
+			iconToUse = <FaYoutube />
+			break;
+		case iconType.PRACTICE:
+			iconToUse = <FaCogs />
+			break;
+		default:
+			iconToUse = <FaFileAlt />
+			break;
+	}
+	return {
+		icon: iconToUse,
+		link: url,
+		label: type
+	};
+}
+
+/**
+ * Helper that makes a url to link a static file in our public/static/classes folder. This link will only work if the file
+ * is placed in the appropriate folder for its class, so if it is not, just make your own link.
+ * @param {number} classNumber the class number
+ * @param {string} fileName the name of the file
+ * @returns a url to that file
+ */
+function makeUrl(classNumber, fileName) {
+	return `${staticBasePath}/${classNumber}/${fileName}`;
+}
+
 const classes = [
     {
 		number: 1, title: "Intro and Classes", outDate: "May 12", active: true,
 		links: [
-		    { icon: <FaBook />,
-		      link: `${staticBasePath}/1/dillo.ml`,
-		      label: "Setup/Prep" },
-		    { icon: <FaFileAlt />,
-		      link: `${staticBasePath}/1/lec01.pdf`,
-		      label: "Notes" },
-		    { icon: <FaYoutube />,
-		      link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa",
-		      label: "Video" },
-		     { icon: <FaFileAlt />,
-		      link: "https://brown-cs18-master.github.io/content/practice/01classes.pdf",
-		      label: "Practice" },
+			makeIcon(iconType.SETUP, makeUrl(1, "dillo.ml")),
+			makeIcon(iconType.NOTES, makeUrl(1, "lec01.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa"),
+			makeIcon(iconType.PRACTICE, makeUrl(1, "01classes.pdf")),
 		],
 		tags: ["Classes"]
     },
     {
 		number: 2, title: "Methods and Testing", outDate: "May 14", active: true,
 		links: [
-		    { icon: <FaFileAlt />,
-		      link: "https://hackmd.io/@cs18-spring-2021/lec02setup",
-		      label: "Setup/Prep" },
-		    //{ icon: <FaFileAlt />,
-		    //  link: "https://cs18-summer-2021.github.io/src/Info/Notes/Classes/lec01.pdf",
-		    //  label: "Notes" },
-		    //{ icon: <FaYoutube />,
-		    //  link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa",
-		    //  label: "Video" },
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec02setup"),
 		],
 		tags: []
 	},
