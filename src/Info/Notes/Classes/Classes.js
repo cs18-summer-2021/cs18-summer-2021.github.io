b45d4f828384c497741cde2f9b50180f68dbef98
ilai2 <isabel_lai@brown.edu>
2021-06-18 17:27:09 -0400

    Fixed class 16 link

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -53,171 +53,171 @@ function makeUrl(classNumber, fileName) {
 }
 
 const classes = [
-    {
-	number: 1, title: "Intro and Classes", outDate: "May 12", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, makeUrl(1, "dillo.ml")),
-	    makeIcon(iconType.NOTES, makeUrl(1, "01classes.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa"),
-	    makeIcon(iconType.PRACTICE, makeUrl(1, "01classes-practice.pdf")),
-	],
-	tags: ["Classes"]
-    },
-    {
-	number: 2, title: "Methods and Testing", outDate: "May 14", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec02setup"),
-	    makeIcon(iconType.NOTES, makeUrl(2, "02methods.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9824c93e-7fbd-4aad-8de7-ad28010b24c9"),
-	    makeIcon(iconType.PRACTICE, makeUrl(2, "02methods-practice.pdf")),
-	],
-	tags: ["Memory Diagram"]
-    },
-    {
-	number: 3, title: "Interfaces and Types", outDate: "May 17", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec03setup"),
-	    makeIcon(iconType.NOTES, makeUrl(3, "03interfaces.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=390f60ec-9112-4550-9122-ad2b010ce733"),
-	    makeIcon(iconType.PRACTICE, makeUrl(3, "03interfaces-practice.pdf")),
-	],
-	tags: []
-    },
-    {
-	number: 4, title: "Abstract Classes", outDate: "May 19", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec04setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(4, "04abs-classes.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=46342dfd-23a2-4591-9132-ad2d010b4057"),
-	    makeIcon(iconType.PRACTICE, makeUrl(4, "04tracing.pdf")),
-	],
-	tags: []
-    },
-    {
-	number: 5, title: "Implementing Lists", outDate: "May 21", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec05setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(5, "05listsfunctional.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=2d037cda-6d4a-4308-a000-ad2f010bf450")
-	],
-	tags: ["Class Hierarchies"]
-    },
-    {
-	number: 6, title: "Changing Fields and Equality", outDate: "May 24", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec06setup"),
-	    makeIcon(iconType.NOTES, makeUrl(6, "06equality.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=574f31ce-be05-4e11-8bce-ad32010a6c53")
-	],
-	tags: ["Memory Diagram"]
-    }, 
-    {
-	number: 7, title: "Java's Built-In Lists", outDate: "May 26", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec07setup"),
-	    makeIcon(iconType.NOTES, makeUrl(7, "07javalists.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=fe0ffbf5-ef76-421c-be6f-ad3401108611")
-	],
-	tags: ["Loops", "Grading"]
-    },
-    {
-	number: 8, title: "Implementing Java Lists", outDate: "May 28", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec08setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(8, "08mutablelists.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9524be5f-e0f3-4b66-8c2e-ad36010abbab")
-	],
-	tags: ["Memory Diagram"]
-    },
-    {
-	number: "N/A", title: "No Class", outDate: "May 31", active: true
-    },
-    {
-	number: 9, title: "Optimizing List Methods", outDate: "Jun 2", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec09setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(9, "09listsoptimize.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=f6414aa1-0cbd-4a2c-b4f0-ad3b010b2cf0")
-	],
-	tags: []
-    },
-    {
-	number: 10, title: "Arrays", outDate: "Jun 4", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec10setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(10, "10arrays.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9b004d25-7365-43b8-9afa-ad3d010ca360")
-	],
-	tags: ["Memory"]
-    },
-    {
-	number: 11, title: "Dynamic Arrays", outDate: "Jun 7", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec11setup"),
-	    makeIcon(iconType.NOTES, makeUrl(11, "11dynArrays.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=8885c2ef-d138-4cae-ae27-ad40010b14ab")
-	],
-	tags: ["Memory"]
-    },
-    {
-	number: 12, title: "Wrap-Around Dynamic Arrays", outDate: "Jun 9", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec12setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(12, "12dynArraysWrap.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ea0cb545-4ac7-4643-9495-ad42010c131b")
-	],
-	tags: []
-    },
-    {
-	number: 13, title: "Enabling For Loops (Iterators)", outDate: "Jun 11", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec13setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(13, "13iterators.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c130d30b-a985-48da-9607-ad44010f0728")
-	],
-	tags: []
-    },
-    {
-	number: 14, title: "Access Modifiers and Program Design", outDate: "Jun 14", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec14setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(14, "14accessEncap.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=2849d09c-877a-4f04-82e5-ad47010d5e1a")
-	],
-	tags: []
-    },
-    {
-	number: "N/A", title: "Midterm Prep Guide", outDate: "Jun 14", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/midterm-prep-sum2021")
-	],
-	tags: []
-    },
-    {
-	number: 15, title: "Exceptions and Model-View-Controller", outDate: "Jun 16", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec15setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(15, "15mvcExceptions.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=702172bc-80b3-4291-bb0c-ad49010be3f2")
-	],
-	tags: ["throw", "try/catch"]
-    },
-    {
-	number: 16, title: "Intro to Hashtables", outDate: "Jun 18", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec16setup-sum"),
-	    makeIcon(iconType.NOTES, makeUrl(16, "hashtables.pdf")),
-	    makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c6abe0c7-ed8f-4a95-80db-ad4b010bc695")
-	],
-	tags: []
-    },
-    {
-	number: 17, title: "Implementing Hashtables", outDate: "Jun 21", active: true,
-	links: [
-	    makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec17setup-sum")
-	],
-	tags: []
-    }
+	{
+		number: 1, title: "Intro and Classes", outDate: "May 12", active: true,
+		links: [
+			makeIcon(iconType.SETUP, makeUrl(1, "dillo.ml")),
+			makeIcon(iconType.NOTES, makeUrl(1, "01classes.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ebcf6e7a-354d-463d-921c-ad26010b74aa"),
+			makeIcon(iconType.PRACTICE, makeUrl(1, "01classes-practice.pdf")),
+		],
+		tags: ["Classes"]
+	},
+	{
+		number: 2, title: "Methods and Testing", outDate: "May 14", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec02setup"),
+			makeIcon(iconType.NOTES, makeUrl(2, "02methods.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9824c93e-7fbd-4aad-8de7-ad28010b24c9"),
+			makeIcon(iconType.PRACTICE, makeUrl(2, "02methods-practice.pdf")),
+		],
+		tags: ["Memory Diagram"]
+	},
+	{
+		number: 3, title: "Interfaces and Types", outDate: "May 17", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec03setup"),
+			makeIcon(iconType.NOTES, makeUrl(3, "03interfaces.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=390f60ec-9112-4550-9122-ad2b010ce733"),
+			makeIcon(iconType.PRACTICE, makeUrl(3, "03interfaces-practice.pdf")),
+		],
+		tags: []
+	},
+	{
+		number: 4, title: "Abstract Classes", outDate: "May 19", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec04setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(4, "04abs-classes.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=46342dfd-23a2-4591-9132-ad2d010b4057"),
+			makeIcon(iconType.PRACTICE, makeUrl(4, "04tracing.pdf")),
+		],
+		tags: []
+	},
+	{
+		number: 5, title: "Implementing Lists", outDate: "May 21", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec05setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(5, "05listsfunctional.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=2d037cda-6d4a-4308-a000-ad2f010bf450")
+		],
+		tags: ["Class Hierarchies"]
+	},
+	{
+		number: 6, title: "Changing Fields and Equality", outDate: "May 24", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec06setup"),
+			makeIcon(iconType.NOTES, makeUrl(6, "06equality.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=574f31ce-be05-4e11-8bce-ad32010a6c53")
+		],
+		tags: ["Memory Diagram"]
+	},
+	{
+		number: 7, title: "Java's Built-In Lists", outDate: "May 26", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec07setup"),
+			makeIcon(iconType.NOTES, makeUrl(7, "07javalists.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=fe0ffbf5-ef76-421c-be6f-ad3401108611")
+		],
+		tags: ["Loops", "Grading"]
+	},
+	{
+		number: 8, title: "Implementing Java Lists", outDate: "May 28", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec08setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(8, "08mutablelists.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9524be5f-e0f3-4b66-8c2e-ad36010abbab")
+		],
+		tags: ["Memory Diagram"]
+	},
+	{
+		number: "N/A", title: "No Class", outDate: "May 31", active: true
+	},
+	{
+		number: 9, title: "Optimizing List Methods", outDate: "Jun 2", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec09setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(9, "09listsoptimize.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=f6414aa1-0cbd-4a2c-b4f0-ad3b010b2cf0")
+		],
+		tags: []
+	},
+	{
+		number: 10, title: "Arrays", outDate: "Jun 4", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec10setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(10, "10arrays.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=9b004d25-7365-43b8-9afa-ad3d010ca360")
+		],
+		tags: ["Memory"]
+	},
+	{
+		number: 11, title: "Dynamic Arrays", outDate: "Jun 7", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec11setup"),
+			makeIcon(iconType.NOTES, makeUrl(11, "11dynArrays.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=8885c2ef-d138-4cae-ae27-ad40010b14ab")
+		],
+		tags: ["Memory"]
+	},
+	{
+		number: 12, title: "Wrap-Around Dynamic Arrays", outDate: "Jun 9", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec12setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(12, "12dynArraysWrap.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=ea0cb545-4ac7-4643-9495-ad42010c131b")
+		],
+		tags: []
+	},
+	{
+		number: 13, title: "Enabling For Loops (Iterators)", outDate: "Jun 11", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec13setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(13, "13iterators.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c130d30b-a985-48da-9607-ad44010f0728")
+		],
+		tags: []
+	},
+	{
+		number: 14, title: "Access Modifiers and Program Design", outDate: "Jun 14", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec14setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(14, "14accessEncap.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=2849d09c-877a-4f04-82e5-ad47010d5e1a")
+		],
+		tags: []
+	},
+	{
+		number: "N/A", title: "Midterm Prep Guide", outDate: "Jun 14", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/midterm-prep-sum2021")
+		],
+		tags: []
+	},
+	{
+		number: 15, title: "Exceptions and Model-View-Controller", outDate: "Jun 16", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec15setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(15, "15mvcExceptions.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=702172bc-80b3-4291-bb0c-ad49010be3f2")
+		],
+		tags: ["throw", "try/catch"]
+	},
+	{
+		number: 16, title: "Intro to Hashtables", outDate: "Jun 18", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec16setup-sum"),
+			makeIcon(iconType.NOTES, makeUrl(16, "16hashtables.pdf")),
+			makeIcon(iconType.VIDEO, "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c6abe0c7-ed8f-4a95-80db-ad4b010bc695")
+		],
+		tags: []
+	},
+	{
+		number: 17, title: "Implementing Hashtables", outDate: "Jun 21", active: true,
+		links: [
+			makeIcon(iconType.SETUP, "https://hackmd.io/@cs18-spring-2021/lec17setup-sum")
+		],
+		tags: []
+	}
 ]
 
 export default classes;
